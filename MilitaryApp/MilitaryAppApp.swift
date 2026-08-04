f46aad135cde861580918b94eacb03a02862aadd
John Dollar <johndollarr@icloud.com>
2026-08-04 08:48:35 -0400

    Initial Commit

---
+++ MilitaryApp/MilitaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MilitaryAppApp.swift
+//  MilitaryApp
+//
+//  Created by John Dollar on 8/4/26.
+//
+
+import SwiftUI
+
+@main
+struct MilitaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
